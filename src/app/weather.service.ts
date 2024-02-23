33fca26f060557f931a4a9174aaf1a4c5d5c4c33
pan1000 <pan.katsaras@gmail.com>
2024-02-23 09:50:56 +0200

    commit without environment files

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from 'src/environments/environment'; 
+
 // Main service that is using the Angular HttpClient module for making HTTP requests to the API.
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData(cityName:string) {
-    return this.http.get("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+environment.API_KEY+"&units=metric")
+    return this.http.get("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+environment.api_key+"&units=metric")
   }
 }
 
